307990297c39026386c1950caf796ca5274d3674
Henrique Tedeschi <henrique.tedeschi@gmail.com>
2020-09-24 23:18:21 -0600

    Finished template (W02)

---
+++ Week 2/includes/nav.php
@@ -0,0 +1,8 @@
+<nav>
+    <a href="#" class="nav-item">Home</a>
+    <a href="#" class="nav-item active">Classic</a>
+    <a href="#" class="nav-item">Sports</a>
+    <a href="#" class="nav-item">SUV</a>
+    <a href="#" class="nav-item">Trucks</a>
+    <a href="#" class="nav-item">Used</a>
+</nav>
