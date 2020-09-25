34de4f743b3ed14c2b527488f3e20f97212d6bd1
Henrique Tedeschi <henrique.tedeschi@gmail.com>
2020-09-24 23:26:35 -0600

    Increase modularization on nav item active (W02)

--- Week 2/includes/nav.php
+++ Week 2/includes/nav.php
@@ -1,6 +1,12 @@
+<?php
+$activePage = null;
+if (isset($currentPage) && !empty($currentPage)) {
+    $activePage = $currentPage;
+}
+?>
 <nav>
-    <a href="#" class="nav-item">Home</a>
-    <a href="#" class="nav-item active">Classic</a>
+    <a href="#" class="nav-item <?php echo ($activePage && $activePage == "home" ? "active" : ""); ?> ">Home</a>
+    <a href="#" class="nav-item">Classic</a>
     <a href="#" class="nav-item">Sports</a>
     <a href="#" class="nav-item">SUV</a>
     <a href="#" class="nav-item">Trucks</a>
